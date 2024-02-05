56ec24f524cd770f9ce11228ae1247394c22fa45
klarajohanss <klara.johansson@elev.ga.ntig.se>
2024-02-05 11:20:11 +0100

    fixed animations, walking, jumping

--- test.rb
+++ test.rb
@@ -1,31 +1,55 @@
 require 'ruby2d'
+set title: 'The End of Day Game', background: 'navy'
+set resizable: true
 
-# Set up the window
-set width: 800, height: 600
-
-# Create a sprite with Idle image
-character_sprite = Sprite.new(
-  'img/Idle.png',   # Path to Idle image
-  x: 0,             # Initial x-coordinate
-  y: 300,           # Initial y-coordinate
-  width: 128        # Clipping width
+skeleton_animation = Sprite.new(
+  'img/skeleton_all_animations.png',
+  clip_width: 128,
+  time: 200,
+  width: 128,
+  height: 128,
+  x: 0, y: 300,
+  animations: {
+    idle: 1..6,
+    walk: 7..14,
+    attack: 15..18,
+    jump: 19..28,
+    hurt: 29..31,
+    dead: 32..34,
+  }
 )
 
-# Key press event handler
+# Set the initial x velocity when walking
+walk_velocity = 2
+
+# Variable to track whether the character is currently walking
+walking_right = false
+walking_left = false
+
 on :key_down do |event|
-  if event.key == 'd'
-    # Change sprite to Walk image when 'd' key is pressed
-    character_sprite.path = 'img/Walk.png'  # Path to Walk image
+  case event.key
+  
   end
 end
 
-# Key release event handler
 on :key_up do |event|
-  if event.key == 'd'
-    # Change sprite back to Idle image when 'd' key is released
-    character_sprite.path = 'img/Idle.png'  # Path to Idle image
+  case event.key
+  when 'a', 'd'
+    skeleton_animation.play animation: :idle, loop: true
+    walking_left = false
+    walking_right = false
   end
 end
 
-# Show the window
+update do
+  if walking_right
+    # Move the character horizontally according to its walking velocity
+    skeleton_animation.x += walk_velocity
+  end
+  if walking_left
+    # Move the character horizontally according to its walking velocity
+    skeleton_animation.x -= walk_velocity
+  end
+end
+
 show
--- the_end_of_day.rb
+++ the_end_of_day.rb
@@ -3,6 +3,7 @@ set title: 'The End of Day Game', background: 'navy'
 set resizable: true
 #Image.new('img/background_og.png')
 
+
 skeleton_animation = Sprite.new(
   'img/skeleton_all_animations.png',
   clip_width: 128,
@@ -20,39 +21,71 @@ skeleton_animation = Sprite.new(
   }
 )
 
-skeleton_default = (skeleton_animation.play animation: :idle, loop: true)
+skeleton_animation.play animation: :idle, loop: true
 
+# Set the initial x velocity when walking
+walk_velocity = 2
+# Variable to track whether the character is currently walking
+walking_right = false
+walking_left = false
+facing_left = false
 
-
-def main()
-  skeleton_default
-
-
-end
-
-on :key_held do |event|
+on :key_up do |event|
   case event.key
-    when 'a'
-      skeleton_animation.play animation: :walk, loop: true, flip: :horizontal
-    when 'd'
-      skeleton_animation.play animation: :walk, loop: true
+    when 'w'
+      skeleton_animation.play animation: :idle, loop: true
+    when'p','h'
+      skeleton_animation.play animation: :idle, loop: true
+    when 'j'
+      skeleton_animation.play animation: :dead, loop: nil #fix animation for dead dead
+    when 'a', 'd'
+      skeleton_animation.play animation: :idle, loop: true
+      walking_left = false
+      walking_right = false
   end
 end
 
+
 on :key_down do |event|
   case event.key
+    when 'a'
+      skeleton_animation.play animation: :walk, loop: true, flip: :horizontal
+      walking_left = true
+      facing_left = true
+    when 'd'
+      skeleton_animation.play animation: :walk, loop: true
+      walking_right = true
+      facing_left = false
     when 'w'
-      skeleton_animation.play animation: :jump, loop: nil
+      if facing_left
+        skeleton_animation.play animation: :jump, loop: false, flip: :horizontal
+      else
+        skeleton_animation.play animation: :jump, loop: false
+      end
+      skeleton_animation.y -= 20
     when 'p'
-      skeleton_animation.play animation: :attack, loop: nil
+      if facing_left
+        skeleton_animation.play animation: :attack, loop: false, flip: :horizontal
+      else
+        skeleton_animation.play animation: :attack, loop: false
+      end
     when 'h'
-      skeleton_animation.play animation: :hurt, loop: nil
+      skeleton_animation.play animation: :hurt, loop: false
     when 'j'
       skeleton_animation.play animation: :dead, loop: nil
   end
 end
 
-
+update do
+  if walking_right
+    # Move the character horizontally according to its walking velocity
+    skeleton_animation.x += walk_velocity
+  end
+  if walking_left
+    # Move the character horizontally according to its walking velocity
+    skeleton_animation.x -= walk_velocity
+  end
+end
 
 
 show
